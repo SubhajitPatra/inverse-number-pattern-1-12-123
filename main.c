63856b2242faf1bb760dc854b12a86249de82877
Subhajit Patra <1souvikpa@gmail.com>
2020-10-11 21:13:37 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    for(a=7;a>0;a=a-1){
+        for(int b=1;b<a;b=b+1){
+            printf("%d",b);
+    }
+        printf("%d\n",a);
+    }
+    return 0;
+}
